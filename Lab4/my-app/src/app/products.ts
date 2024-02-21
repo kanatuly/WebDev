072341334cdf9336c996a9d2f7513d7ba1397aad
Amanbol Kanatuly <amanbol9kanatuly@gmail.com>
2024-02-21 22:19:41 +0600

    done

---
+++ Lab4/my-app/src/app/products.ts
@@ -0,0 +1,148 @@
+export interface Product {
+  image: string;
+  name: string;
+  description: string;
+  price: number;
+  link: string;
+}
+
+export const products = [
+  {
+    image: "/assets/images/img1.png",
+    name: "Apple AirPods 3",
+    description: "тип: гарнитура\n" +
+      "вид: внутриканальные\n" +
+      "подключение: беспроводное\n" +
+      "тип акустического оформления: открытые\n" +
+      "тип крепления: без крепления\n" +
+      "система активного шумоподавления: Нет\n" +
+      "микрофон: Да",
+    price: 85998,
+    link: "https://kaspi.kz/shop/p/apple-airpods-3-with-lightning-charging-case-belyi-106667987/?c=233620100"
+  },
+  {
+    image: "/assets/images/img2.png",
+    name: "Marshall Major",
+    description: "тип: гарнитура\n" +
+      "вид: накладные\n" +
+      "подключение: беспроводное\n" +
+      "тип акустического оформления: закрытые\n" +
+      "тип крепления: оголовье\n" +
+      "система активного шумоподавления: Нет\n" +
+      "микрофон: Да",
+    price: 69737,
+    link: "https://kaspi.kz/shop/p/marshall-major-iv-chernyi-102138144/?c=233620100"
+  },
+  {
+    image: "/assets/images/img3.png",
+    name: "Realme Buds T100",
+    description: "тип: гарнитура\n" +
+      "вид: внутриканальные\n" +
+      "подключение: беспроводное\n" +
+      "тип акустического оформления: закрытые\n" +
+      "тип крепления: без крепления\n" +
+      "система активного шумоподавления: Нет\n" +
+      "микрофон: Да",
+    price: 9287,
+    link: "https://kaspi.kz/shop/p/realme-buds-t100-chernyi-107357596/?c=233620100"
+  },
+  {
+    image: "/assets/images/img4.png",
+    name: "TWS F9-5",
+    description: "тип: гарнитура\n" +
+      "вид: внутриканальные\n" +
+      "подключение: беспроводное\n" +
+      "тип акустического оформления: полуоткрытые\n" +
+      "тип крепления: без крепления\n" +
+      "система активного шумоподавления: Нет\n" +
+      "микрофон: Да",
+    price: 1188,
+    link: "https://kaspi.kz/shop/p/tws-f9-5-chernye-101769530/?c=233620100"
+  },
+  {
+    image: "/assets/images/img5.png",
+    name: "CAT EAR STN-28",
+    description: "тип: гарнитура\n" +
+      "вид: накладные\n" +
+      "подключение: беспроводное\n" +
+      "тип акустического оформления: закрытые\n" +
+      "тип крепления: оголовье\n" +
+      "система активного шумоподавления: Нет\n" +
+      "микрофон: Да",
+    price: 1897,
+    link: "https://kaspi.kz/shop/p/cat-ear-stn-28-rozovyi-101768468/?c=233620100"
+  },
+  {
+    image: "/assets/images/img6.png",
+    name: "Samsung Galaxy Buds 2 SM-R177NZKACIS",
+    description: "тип: гарнитура\n" +
+      "вид: внутриканальные\n" +
+      "подключение: беспроводное\n" +
+      "тип акустического оформления: закрытые\n" +
+      "тип крепления: без крепления\n" +
+      "система активного шумоподавления: Да\n" +
+      "микрофон: Да",
+    price: 36987,
+    link: "https://kaspi.kz/shop/p/samsung-galaxy-buds-2-sm-r177nzkacis-chernyi-102046373/?c=233620100"
+  },
+  {
+    image: "/assets/images/img7.png",
+    name: "Remax 10 Plus",
+    description: "тип: наушники\n" +
+      "вид: внутриканальные\n" +
+      "подключение: беспроводное\n" +
+      "тип акустического оформления: закрытые\n" +
+      "тип крепления: без крепления\n" +
+      "система активного шумоподавления: Нет\n" +
+      "микрофон: Да",
+    price: 5988,
+    link: "https://kaspi.kz/shop/p/remax-10-plus-belyi-103380508/?c=233620100"
+  },
+  {
+    image: "/assets/images/img8.png",
+    name: "Realme Buds T100",
+    description: "тип: гарнитура\n" +
+      "вид: внутриканальные\n" +
+      "подключение: беспроводное\n" +
+      "тип акустического оформления: закрытые\n" +
+      "тип крепления: без крепления\n" +
+      "система активного шумоподавления: Нет\n" +
+      "микрофон: Да",
+    price: 9954,
+    link: "https://kaspi.kz/shop/p/realme-buds-t100-sinii-109855444/?c=233620100"
+  },
+  {
+    image: "/assets/images/img9.png",
+    name: "Apple AirPods Max",
+    description:"тип: гарнитура\n" +
+      "вид: накладные\n" +
+      "подключение: беспроводное\n" +
+      "тип акустического оформления: закрытые\n" +
+      "тип крепления: оголовье\n" +
+      "система активного шумоподавления: Да\n" +
+      "микрофон: Да",
+    price: 273325,
+    link: "https://kaspi.kz/shop/p/apple-airpods-max-serebristyi-100949286/?c=233620100"
+  },
+  {
+    image: "/assets/images/img10.png",
+    name: "Apple EarPods USB-C",
+    description:"тип: гарнитура\n" +
+      "вид: внутриканальные\n" +
+      "подключение: с проводом\n" +
+      "тип акустического оформления: открытые\n" +
+      "тип крепления: без крепления\n" +
+      "система активного шумоподавления: Нет\n" +
+      "микрофон: Да" ,
+    price: 7944 ,
+    link: "https://kaspi.kz/shop/p/apple-earpods-usb-c-belyi-114086432/?c=233620100"
+  },
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
